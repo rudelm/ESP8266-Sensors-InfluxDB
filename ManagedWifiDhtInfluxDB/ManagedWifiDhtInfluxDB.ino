4cd14a39cad9d883539ba0b5db18db8ff7309c05
Markus Rudel <markus.rudel@ruhr-uni-bochum.de>
2018-07-13 21:17:20 +0200

    new ManagedWifiDhtInfluxDB project added, more readme

---
+++ ManagedWifiDhtInfluxDB/ManagedWifiDhtInfluxDB.ino
@@ -0,0 +1,174 @@
+// code taken from https://github.com/tzapu/WiFiManager/blob/master/examples/AutoConnectWithFSParameters/AutoConnectWithFSParameters.ino
+
+#include <FS.h>                   // this needs to be first, or it all crashes and burns...
+#include <ESP8266WiFi.h>          // ESP8266 Core WiFi Library (you most likely already have this in your sketch)
+
+#include <DNSServer.h>            // Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     // Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>          // https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+
+#include <ESPinfluxdb.h>          // InfluxDB wrapper
+#include <DHT.h>                  // Library for DHT22 temperature sensor
+#include <DHT_U.h>
+#include <ArduinoJson.h>          // https://github.com/bblanchon/ArduinoJson
+
+#define DHTPIN 12                  // DHT connection on GPIO Pin 12 or D6 of NodeMCU LoLin V3
+#define DHTTYPE DHT22              // DHT 22
+
+// define your default values here, if there are different values in config.json, they are overwritten.
+char influxdb_server[60];
+char influxdb_port[6] = "8086";
+char influxdb_db[30] = "default";
+char influxdb_user[30];
+char influxdb_password[30];
+char measurement[30] = "climate";
+
+//flag for saving data
+bool shouldSaveConfig = false;
+
+//callback notifying us of the need to save config
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+
+  //clean FS, for testing
+  // SPIFFS.format();
+
+  //read configuration from FS json
+  Serial.println("mounting FS...");
+
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+          Serial.println("\nparsed json");
+
+          strcpy(influxdb_server, json["influxdb_server"]);
+          strcpy(influxdb_port, json["influxdb_port"]);
+          strcpy(influxdb_db, json["influxdb_db"]);
+          strcpy(influxdb_user, json["influxdb_user"]);
+          strcpy(influxdb_password, json["influxdb_password"]);
+          strcpy(measurement, json["measurement"]);
+
+        } else {
+          Serial.println("failed to load json config");
+        }
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+  //end read
+
+  // The extra parameters to be configured (can be either global or just in the setup)
+  // After connecting, parameter.getValue() will get you the configured value
+  // id/name placeholder/prompt default length
+  WiFiManagerParameter custom_influxdb_server("server", "InfluxDB server", influxdb_server, 60);
+  WiFiManagerParameter custom_influxdb_port("port", "InfluxDB server port", influxdb_port, 6);
+  WiFiManagerParameter custom_influxdb_db("db", "InfluxDB database", influxdb_db, 30);
+  WiFiManagerParameter custom_influxdb_user("user", "InfluxDB username", influxdb_user, 30);
+  WiFiManagerParameter custom_influxdb_password("password", "InfluxDB userpassword", influxdb_password, 30);
+  WiFiManagerParameter custom_measurement("measurement", "InfluxDB measurement", measurement, 30);
+
+  // WiFiManager
+  // Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+
+  // set config save notify callback
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+  // set static ip
+  //wifiManager.setSTAStaticIPConfig(IPAddress(10,0,1,99), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+
+  // add all your parameters here
+  wifiManager.addParameter(&custom_influxdb_server);
+  wifiManager.addParameter(&custom_influxdb_port);
+  wifiManager.addParameter(&custom_influxdb_db);
+  wifiManager.addParameter(&custom_influxdb_user);
+  wifiManager.addParameter(&custom_influxdb_password);
+  wifiManager.addParameter(&custom_measurement);
+
+  // reset settings - for testing
+  // wifiManager.resetSettings();
+
+  // set minimu quality of signal so it ignores AP's under that quality
+  // defaults to 8%
+  //wifiManager.setMinimumSignalQuality();
+
+  // sets timeout until configuration portal gets turned off
+  // useful to make it all retry or go to sleep
+  // in seconds
+  wifiManager.setTimeout(300);
+
+  // fetches ssid and pass and tries to connect
+  // if it does not connect it starts an access point with the specified name
+  // here  "DHT22-Sensor"
+  // and goes into a blocking loop awaiting configuration
+  if (!wifiManager.autoConnect("DHT22-Sensor", "configureMe")) {
+    Serial.println("failed to connect and hit timeout");
+    delay(3000);
+    // reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(5000);
+  }
+
+  // if you get here you have connected to the WiFi
+  Serial.println("connected to the selected WiFi...");
+
+  // read updated parameters
+  strcpy(influxdb_server, custom_influxdb_server.getValue());
+  strcpy(influxdb_port, custom_influxdb_port.getValue());
+  strcpy(influxdb_db, custom_influxdb_db.getValue());
+  strcpy(influxdb_user, custom_influxdb_user.getValue());
+  strcpy(influxdb_password, custom_influxdb_password.getValue());
+  strcpy(measurement, custom_measurement.getValue());
+
+  // save the custom parameters to FS
+  if (shouldSaveConfig) {
+    Serial.println("saving config");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["influxdb_server"] = influxdb_server;
+    json["influxdb_port"] = influxdb_port;
+    json["influxdb_db"] = influxdb_db;
+    json["influxdb_user"] = influxdb_user;
+    json["influxdb_password"] = influxdb_password;
+    json["measurement"] = measurement;
+    
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {
+      Serial.println("failed to open config file for writing");
+    }
+
+    json.printTo(Serial);
+    json.printTo(configFile);
+    configFile.close();
+    //end save
+  }
+
+  Serial.println("local ip");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # ESP8266 and InfluxDB sketches
-- ESP8266 and DHT22
+This is a collection of ESP8266 and DHT22 sketches I'm using in my home. While the `DHTInfluxDB` project was from the forked repo, the `ManagedWifiDhtInfluxDB` project is a new development up from the ground.
 
 # Dependencies
 - https://github.com/esp8266/Arduino
 - https://github.com/adafruit/DHT-sensor-library
 - https://github.com/adafruit/Adafruit_Sensor
 - https://github.com/hwwong/ESP_influxdb
+- https://github.com/tzapu/WiFiManager
+- https://github.com/bblanchon/ArduinoJson
+- https://github.com/adafruit/Adafruit_SPIFlash
+
+If you're using Mac OS High Sierra, you'll need [working](https://www.reddit.com/r/arduino/comments/7cq68i/any_new_drivers_for_ch340g_on_mac_os_high_sierra/) USB Serial drivers for the CH340G chip.
 
 # Pinout
 I'm using an ESP8266 NodeMCU Lolin v3. The DHTPIN points to 12, which is the GPIO Pin 12 and therefore the Pin D6:
@@ -15,3 +20,10 @@ I'm using an ESP8266 NodeMCU Lolin v3. The DHTPIN points to 12, which is the GPI
 Image taken from https://github.com/opendata-stuttgart/meta/wiki/Pinouts-NodeMCU-v2,-v3
 
 You should also be able to use the Arduino Pin Definitions directly, so this would be D6 instead of 12.
+
+# Setup
+If you use the `ManagedWifiDhtInfluxDB` project, you'll get a new Wifi AP in your network list called `DHT22-Sensor`. You can connect to this Wifi with password `configureMe`. After entering these credentials, a Webpage will be shown to you. This is similar to the captive portals you might know from public hotpots. After you've entered all necessary information, the settings will be persisted and it will connect to the new Wifi as client. 
+
+If you don't connect to the AP, it will disconnect after 5 minutes. You need to restart reconnect to the Wifi for another configuration run.
+
+If you've ever used the ESP8266 for anything else, it might happen, that you still have a Wifi Configuration stored. In this case, comment out the wifiManager.resetSettings() method as well as the SPIFFS.format() method to clear the memory. After upload comment those methods back in and flash again.
